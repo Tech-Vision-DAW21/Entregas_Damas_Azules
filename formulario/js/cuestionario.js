6d13b9c284d6ee096cc04659aa6e2b3191fb6cd5
RazielHR <RazielHR@users.noreply.github.com>
2021-03-18 15:01:38 -0600

    Modificaciones

--- formulario/js/cuestionario.js
+++ formulario/js/cuestionario.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function(){
     var date_input=$('input[name="date"]'); //our date input has the name "date"
     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
@@ -10,13 +10,3 @@ $(document).ready(function(){
     };
     date_input.datepicker(options);
   })
-=======
-
-function cerrar(){
-    window.location.href="../login/login.html";
-}
-
-function siguiente(){
-    window.location.href="../formulario_2/cuestionario_2.html";
-}
->>>>>>> e63b1219dec0db6267c8e2d6f146aa6486b102d0
